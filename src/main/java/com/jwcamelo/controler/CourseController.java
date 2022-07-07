577a1c0e6f88eee3309209ae949c2f0650be90e5
Jhonatan Wesley Camêlo <jwcmade@gmail.com>
2022-07-07 01:25:38 -0300

    hello world

---
+++ src/main/java/com/jwcamelo/controler/CourseController.java
@@ -0,0 +1,17 @@
+package com.jwcamelo.controler;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+  @GetMapping
+  public List<Object> list() {
+    return null;
+  }
+}
