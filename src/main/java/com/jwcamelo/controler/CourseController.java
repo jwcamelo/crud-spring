8165c57b6d1c2ecbf0cbb5700e4863b1771c09bc
Jhonatan Wesley Camêlo <jwcmade@gmail.com>
2022-07-08 00:26:18 -0300

    add method post

--- src/main/java/com/jwcamelo/controler/CourseController.java
+++ src/main/java/com/jwcamelo/controler/CourseController.java
@@ -2,8 +2,13 @@ package com.jwcamelo.controler;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jwcamelo.model.Course;
@@ -22,5 +27,13 @@ public class CourseController {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  // @RequestMapping(method = RequestMethod.POST)
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    // return courseRepository.save(course);
+    return courseRepository.save(course);
   }
 }
